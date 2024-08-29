9d9188653258b5b32e19a772527b05b437ccb78b
rastko <milosevic.rastko@yahoo.com>
2024-08-29 19:48:20 +0200

    "Added more DGS endpoints"
    Task: #7

--- poc/src/main/java/com/dgsApp/poc/apiLayer/UserDataFetcher.java
+++ poc/src/main/java/com/dgsApp/poc/apiLayer/UserDataFetcher.java
@@ -3,7 +3,9 @@ package com.dgsApp.poc.apiLayer;
 import com.dgsApp.poc.serviceLayer.dto.UserDto;
 import com.dgsApp.poc.serviceLayer.services.interfaces.UserService;
 import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.DgsQuery;
+import com.netflix.graphql.dgs.InputArgument;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -13,12 +15,32 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserDataFetcher {
 
-    private UserService userService;
+    private final UserService userService;
 
     @DgsQuery(field = "user")
-    public List<UserDto> getAllUsers(){
+    public UserDto getUserById(@InputArgument Long id) {
+        return userService.findById(id);
+    }
+
+
+    @DgsQuery(field = "users")
+    public List<UserDto> getAllUsers() {
         return userService.findAll();
     }
 
+    @DgsMutation(field = "createUser")
+    public UserDto createUser(@InputArgument String name, @InputArgument String email) {
+        return userService.save(new UserDto());
+    }
+
+    @DgsMutation(field = "updateUser")
+    public UserDto updateUser(@InputArgument Long id, @InputArgument String name, @InputArgument String email) {
+        return userService.update(id, new UserDto(id, name, email));
+    }
+
+    @DgsMutation(field = "deleteUser")
+    public void deleteUser(@InputArgument Long id) {
+        userService.deleteById(id);
+    }
 
 }
--- poc/src/main/java/com/dgsApp/poc/serviceLayer/dto/UserDto.java
+++ poc/src/main/java/com/dgsApp/poc/serviceLayer/dto/UserDto.java
@@ -1,10 +1,13 @@
 package com.dgsApp.poc.serviceLayer.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.UUID;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     private Long id;
     private String name;
