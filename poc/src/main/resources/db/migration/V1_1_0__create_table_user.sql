a10b2c2eb244d8b38004295f667c7963101f208c
rastko <milosevic.rastko@yahoo.com>
2024-08-30 04:04:53 +0200

    "Added mysql support"
    Task: #11

--- poc/src/main/resources/db/migration/V1_1_0__create_table_user.sql
+++ poc/src/main/resources/db/migration/V1_1_0__create_table_user.sql
@@ -1,5 +1,6 @@
-CREATE TABLE IF NOT EXISTS 'user' (
-    'id' INTEGER PRIMARY KEY AUTOINCREMENT,
-    'name' varchar(50),
-    'email' varchar(100)
+CREATE TABLE IF NOT EXISTS user (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name varchar(50),
+    email varchar(100),
+    PRIMARY KEY (id)
 )
